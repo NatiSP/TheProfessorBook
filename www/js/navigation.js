97d315c0750e847e034307b61b792c7afe6361d0
Natalia Gutierrez <nataliagtrd@gmail.com>
2018-07-09 17:26:20 +0200

    Timer mejorado, log con debug

--- www/js/navigation.js
+++ www/js/navigation.js
@@ -1,3 +1,5 @@
+var timerGage;
+
 // process the confirmation dialog result
 function onConfirm(buttonIndex) {
     if(buttonIndex == "Yes"){
@@ -23,7 +25,7 @@ function hideAll(){
   $("#rulesTab").addClass("d-none");
   $("#timerTab").addClass("d-none");
   $("#HPCalcTab").addClass("d-none");
-  $("#penaltyTab").addClass("d-none");  
+  $("#penaltyTab").addClass("d-none");
   $("#penaltyForm").addClass("d-none");
   $("#penaltyExcel").addClass("d-none");
   $("#settingsTab").addClass("d-none");
@@ -45,6 +47,31 @@ function loadTimer(){
   hideAll();
   $("#timerTab").removeClass("d-none");
   $('.menu').toggleClass('menu-active');
+  if(timerGage == null){
+    timerGage = new JustGage({
+      id: "gg1",
+      value: 3000,
+      min: 0,
+      max: 3000,
+      hideMinMax: true,
+      donut: true,
+      pointer: true,
+      counter: true,
+      customSectors: [{
+        color : "#ff0000",
+        lo : 0,
+        hi : 300
+      },{
+        color : "#00ff00",
+        lo : 300,
+        hi : 3000
+      }],
+      textRenderer: getCurrentTime,
+      relativeGaugeSize: true
+    });
+    timerGage.refresh(3000);
+  }
+
 }
 
 function loadHPCalc(){
@@ -54,7 +81,7 @@ function loadHPCalc(){
 }
 
 function loadPenalties(){
-	hideAll();	
+	hideAll();
 	$("#penaltyTab").trigger("loadListPenalty");
 	$('.menu').toggleClass('menu-active');
 }
@@ -75,6 +102,21 @@ if($("#indexTab").hasClass("d-none")){
 
 }
 
+var getCurrentTime = function (){
+  var minutes = Math.floor(3000 / 60);
+  var seconds = 3000 - minutes * 60;
+  return (new Array(2+1).join('0')+minutes).slice(-2) + ':' + (new Array(2+1).join('0')+seconds).slice(-2);
+}
+
 $("#button").click(function() {
   $('.menu').toggleClass('menu-active');
+});
+
+// setup a logfile path (required)
+// this path is relative to your device sdcard storage directory
+window.logToFile.setLogfilePath('/ProfessorBook/log.txt', function () {
+    // logger configured successfully
+}, function (err) {
+    // logfile could not be written
+    // handle error
 });
--- www/js/timer.js
+++ www/js/timer.js
@@ -1,6 +1,7 @@
 
 professorApp.controller("timerCtrl", ['$scope','$timeout',
   function($scope,$timeout) {
+
       var storage = window.localStorage;
       $scope.counter = 3000;
       $scope.time = 100 ;
@@ -12,7 +13,14 @@ professorApp.controller("timerCtrl", ['$scope','$timeout',
         storage.setItem("statusTimer", $scope.status);
       }
       $scope.startTime = Date.now();
+      $scope.timerGage = timerGage;
       $scope.start = function() {
+        if(typeof $scope.timerGage != 'undefined' && $scope.timerGage != null){
+          //$scope.timerGage.refresh($scope.counter, $scope.timeLimit);
+          $scope.timerGage.refresh($scope.counter);
+        } else {
+          $scope.timerGage = timerGage;
+        }
         if($scope.status != "start"){
           $scope.status = "start";
           storage.setItem("statusTimer", $scope.status);
@@ -23,7 +31,13 @@ professorApp.controller("timerCtrl", ['$scope','$timeout',
           $scope.startTime = storage.getItem("startTime");
         }
         stopped = $timeout(function() {
-          if($scope.counter > 0){
+          if($scope.counter == 0){
+            window.logToFile.debug("Timer ended!");
+            console.log("Timer ended!");
+            navigator.vibrate(3000);
+          }
+          //if($scope.counter > 0){
+            window.logToFile.debug($scope.counter + " " + $scope.time);
             console.log($scope.counter + " " + $scope.time);
             $scope.counter = $scope.timeLimit - (((new Date().getTime() - $scope.startTime) / 1000) | 0);
             $scope.time = 100 * ($scope.counter/$scope.timeLimit);
@@ -31,7 +45,7 @@ professorApp.controller("timerCtrl", ['$scope','$timeout',
                 $scope.startTime = Date.now() + 1000;
             }
             $scope.start();
-          }
+          //}
         }, 1000);
       };
       if($scope.status == 'start'){
@@ -50,6 +64,10 @@ professorApp.controller("timerCtrl", ['$scope','$timeout',
           mytimeout = $timeout($scope.onTimeout,1000);
           $scope.time = $scope.counter * 100 / $scope.timeLimit;
           $scope.startTime = Date.now();
+          if(typeof $scope.timerGage == 'undefined' || $scope.timerGage == null){
+            $scope.timerGage = timerGage;
+          }
+          $scope.timerGage.refresh($scope.counter, $scope.timeLimit);
       }
 
       $scope.setTime= function(timeSet){
@@ -58,6 +76,36 @@ professorApp.controller("timerCtrl", ['$scope','$timeout',
           $scope.timeLimit = timeSet;
           $scope.reset();
         }
+      }
+
+      getCurrentTime = function(time){
+        var sign = "";
+        if(time == null){
+          if($scope.counter >= 0){
+            var minutes = Math.floor($scope.counter / 60);
+            var seconds = $scope.counter - minutes * 60;
+          }
+          else{
+            var minutes = Math.abs(Math.ceil($scope.counter / 60));
+            var seconds = Math.abs($scope.counter - minutes * 60);
+            sign = "-";
+          }
+        } else {
+          if(time >= 0){
+            var minutes = Math.floor(time / 60);
+            var seconds = Math.abs(time - minutes * 60);
+          }
+          else{
+            var minutes = Math.abs(Math.ceil(time / 60));
+            var seconds = Math.abs(time - minutes * 60);
+            sign = "-";
+          }
+
+        }
+        window.logToFile.debug("Time:" + sign + minutes + ':' + ((seconds < 10) ? '0' + seconds : seconds));
+        console.log("Time:" + sign + minutes + ':' + ((seconds < 10) ? '0' + seconds : seconds));
+        return sign + minutes + ':' + ((seconds < 10) ? '0' + seconds : seconds);
+        //return (new Array(2+1).join('0')+minutes).slice(-2) + ':' + (new Array(2+1).join('0')+seconds).slice(-2);
       }
 
   }]);
